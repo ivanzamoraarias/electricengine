4622957adf67a8c229d54df5914a1f5b6557e4f2
ivanzamoraarias <faucar93@gmail.com>
2017-09-05 18:56:39 -0500

    Object boxing gameobject

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ serv.listen(2000);
 console.log('Server Started at port 2000');
 
 var socketList={};
-var playerList={};
+//var playerList={};
 
 
 var GameObject= function(){
@@ -40,51 +40,47 @@ var GameObject= function(){
     }
     return self;
 };
+
+//Player Class
 var Player = function(id){
-    var self={
-        x:250,
-        y:250,
-        id:id,
-        number:""+Math.floor(10*Math.random()),
+    var self=GameObject();
+    self.id=id;
+    self.number=""+Math.floor(10*Math.random());
+    self.pressingRight=false;
+    self.pressingLeft=false;
+    self.pressingUp=false;
+    self.pressingDown=false;
+    self.maxSpeed=10;
 
-        pressingRight:false,
-        pressingLeft:false,
-        pressingUp:false,
-        pressingDown:false,
-
-        maxSpeed:10,
+    var parentUpdate= self.update;
+    self.update=function(){
+        self.updateVelocity();
+        parentUpdate();
     }
-    self.updatePosition=function(){
+
+    self.updateVelocity=function(){
         if(self.pressingRight)
-            self.x += self.maxSpeed;
-        if(self.pressingLeft)
-            self.x -= self.maxSpeed;
+            self.vx = self.maxSpeed;
+        else if(self.pressingLeft)
+            self.vx = -self.maxSpeed;
+        else
+            self.vx=0;
         if(self.pressingUp)
-            self.y -= self.maxSpeed;
-        if(self.pressingDown)
-            self.y += self.maxSpeed;
-
+            self.vy = -self.maxSpeed;
+        else if(self.pressingDown)
+            self.vy = self.maxSpeed;
+        else self.vy=0;
     }
+
+    //static variable for contain all the players
+    Player.List[id]=self;
     return self;
 }
-
-
-
-//all the sintacxis for socket io , for receive and emit
-var io = require('socket.io')(serv,{});
-io.sockets.on('connection', function (socket) {
-    console.log('socket connection');
-
-    socket.id= Math.random();
-    socketList[socket.id]=socket;
-
+//Static Variable
+Player.List={};
+//Static Function
+Player.onConnect=function(socket){
     var player = Player(socket.id);
-    playerList[socket.id]= player;
-
-    socket.on('disconnect',function(){
-        delete socketList[socket.id];
-        delete  playerList[socket.id];
-    });
     socket.on('keyPress', function (data) {
         if(data.inputId === 'left')
             player.pressingLeft = data.state;
@@ -95,17 +91,17 @@ io.sockets.on('connection', function (socket) {
         else if(data.inputId === 'down')
             player.pressingDown = data.state;
     });
+}
 
-
-});
-
-//game loop
-setInterval(function () {
+Player.onDisconnect=function(socket){
+    delete  Player.List[socket.id];
+}
+Player.update=function(){
     var objectList=[];
-    for(var i in playerList)// the i will let the index of the object in the list
+    for(var i in Player.List)// the i will let the index of the object in the list
     {
-        var player= playerList[i];
-        player.updatePosition();
+        var player= Player.List[i];
+        player.update();
         objectList.push({
             x:player.x,
             y:player.y,
@@ -113,6 +109,37 @@ setInterval(function () {
         });
 
     }
+    return objectList;
+}
+
+//Bullet Class
+var Bullet= function(angle){
+    var self=GameObject();
+    self.id=Math.random();
+    self.maxSpeed=10;
+    self.vx=Math.cos(angle/180*Math.PI)*10;//10 es igual a self.maxspeed
+    self.vy=Math.sin(angle/180*Math.PI)*10;
+
+}
+
+//all the sintacxis for socket io , for receive and emit
+var io = require('socket.io')(serv,{});
+io.sockets.on('connection', function (socket) {
+    console.log('socket connection');
+
+    socket.id= Math.random();
+    socketList[socket.id]=socket;
+    Player.onConnect(socket);
+
+    socket.on('disconnect',function(){
+        delete socketList[socket.id];
+        Player.onDisconnect(socket);
+    });
+});
+
+//game loop
+setInterval(function () {
+    var objectList=Player.update();
     for(var i in socketList){
         var s= socketList[i];
         s.emit('newPosition',objectList);
